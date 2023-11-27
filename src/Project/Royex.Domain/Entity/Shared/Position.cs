78e4d88c7aeb69dad7db35898efb1362ad464558
sojib <mdsojibhosen444@gmail.com>
2023-11-27 18:41:34 +0600

    Complete 1.1

--- src/Project/Royex.Domain/Entity/Shared/Position.cs
+++ src/Project/Royex.Domain/Entity/Shared/Position.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Royex.Domain.Entity.Shared
+﻿namespace Royex.Domain.Entity.Shared
 {
     public enum Position
     {
