9bc060c403fefc131af2f336c04aabd288ce14f2
sojib <mdsojibhosen444@gmail.com>
2023-11-27 18:08:35 +0600

    Adding Migration for seeding more data

--- src/Project/Royex.Persistance/Context/Configuration/EmployeeConfiguration.cs
+++ src/Project/Royex.Persistance/Context/Configuration/EmployeeConfiguration.cs
@@ -58,6 +58,55 @@ namespace Royex.Persistance.Context.Configuration
                 Position = Domain.Entity.Shared.Position.OfficeExecutive,
                 Salary = 200,
                 JoiningDate = new DateTime(2020, 8, 9),
+            },
+            new Employee()
+            {
+                Id = 7,
+                Name = "Tom",
+                Position = Domain.Entity.Shared.Position.GeneralManager,
+                Salary = 200,
+                JoiningDate = new DateTime(2012, 1, 1),
+                IsBonusAdded = true,
+            },
+            new Employee()
+            {
+                Id = 8,
+                Name = "Asik",
+                Position = Domain.Entity.Shared.Position.OfficeExecutive,
+                Salary = 100,
+                JoiningDate = new DateTime(2023, 5, 3),
+            },
+            new Employee()
+            {
+                Id = 9,
+                Name = "Asad",
+                Position = Domain.Entity.Shared.Position.GeneralManager,
+                Salary = 100,
+                JoiningDate = new DateTime(2011,7, 20),
+            },
+            new Employee()
+            {
+                Id = 10,
+                Name = "Haskil",
+                Position = Domain.Entity.Shared.Position.OfficeExecutive,
+                Salary = 100,
+                JoiningDate = new DateTime(2019, 4, 13),
+            },
+            new Employee()
+            {
+                Id = 11,
+                Name = "Comok",
+                Position = Domain.Entity.Shared.Position.GeneralManager,
+                Salary = 200,
+                JoiningDate = new DateTime(2000, 5, 20),
+            },
+            new Employee()
+            {
+                Id = 12,
+                Name = "Mijan",
+                Position = Domain.Entity.Shared.Position.OfficeExecutive,
+                Salary = 100,
+                JoiningDate = new DateTime(2015, 8, 4),
             });
 
         }
---
+++ src/Project/Royex.Presentation/Migrations/20231127120703_AddSomeMore-data-for_seed.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Royex.Presentation.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSomeMoredatafor_seed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "IsBonusAdded", "JoiningDate", "Name", "Position", "Salary" },
+                values: new object[,]
+                {
+                    { 7, true, new DateTime(2012, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Tom", 1, 200 },
+                    { 8, false, new DateTime(2023, 5, 3, 0, 0, 0, 0, DateTimeKind.Unspecified), "Asik", 2, 100 },
+                    { 9, false, new DateTime(2011, 7, 20, 0, 0, 0, 0, DateTimeKind.Unspecified), "Asad", 1, 100 },
+                    { 10, false, new DateTime(2019, 4, 13, 0, 0, 0, 0, DateTimeKind.Unspecified), "Haskil", 2, 100 },
+                    { 11, false, new DateTime(2000, 5, 20, 0, 0, 0, 0, DateTimeKind.Unspecified), "Comok", 1, 200 },
+                    { 12, false, new DateTime(2015, 8, 4, 0, 0, 0, 0, DateTimeKind.Unspecified), "Mijan", 2, 100 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 12);
+        }
+    }
+}
